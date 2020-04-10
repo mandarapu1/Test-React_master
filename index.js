bf892973437c1d217f8a3d65b4d230f15573e216
mandarapu1 <49420950+mandarapu1@users.noreply.github.com>
2020-04-10 18:17:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//import movies from './components/movies.jsx';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
